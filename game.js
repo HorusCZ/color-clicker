04c066af2ee35a4e2af69da6a073525e4b023962
HorusCZ <83790425+HorusCZ@users.noreply.github.com>
2023-11-25 00:08:46 +0100

    added intro

--- game.js
+++ game.js
@@ -1,10 +1,13 @@
 const matrixSize = 8;
+const introSteps = 12;
+const state = { ready: false, clicked: false };
 
 const randomColor = () => {
     return "#" + Math.floor(Math.random()*16777184).toString(16);
 };
 
-const shatterItem = (target) => {
+const shatterItem = (target, forced = false) => {
+    if (!forced && !state.ready) return;
     target.classList.add("no-pointer-events");
     [...target.querySelectorAll(".item")].map(s => {
         s.classList.remove("maximized");
@@ -45,14 +48,37 @@ const newStain = (x, y) => {
     }, 100);
 };
 
+const intro = (i = 0) => {
+  if (i < 0) return;
+  let y = Math.floor(Math.random()*window.innerHeight);
+  let x = Math.floor(Math.random()*window.innerWidth);  
+  let stain = newStain(x, y);
+  setTimeout(() => { shatterItem(stain, true); }, 800);
+  if (i > 0) {
+    setTimeout(() => { intro(i - 1); }, 200);
+  }
+  if (i == 0) {
+    setTimeout(() => {
+      document.querySelector("#container > span").style.opacity = 1;
+      state.ready = true;
+    }, 1800);
+  }
+};
+
 window.addEventListener("load", () => {
     let action = (event) => {
+        if (!state.ready) return;
         event = event || window.event;
         let source = event.target || event.srcElement;
         if (source.id == "container") {
+          if (!state.clicked) {
+            state.clicked = true;
+            document.querySelector("#container > span").style.opacity = 0.2;
+          }
           newStain(event.pageX, event.pageY);
         }
     };
     document.getElementById("container").onclick = action;
     document.getElementById("container").ontouchstart = action;
+    intro(introSteps);
 });
